35274a669073349d2d0ffa6c1c0bd22a74d45bed
Igor G <io.gaev@gmail.com>
2018-04-06 22:43:34 +0500

    master

--- admin.php
+++ admin.php
@@ -1,17 +1,34 @@
+<?php
+if (!empty($_FILES)) {
+    $json = "json";
+
+    $fileName = $_FILES['testfile']['name'];
+    $explode = explode(".", $fileName);
+
+    if ($explode[1] !== $json) {
+        echo 'Можно загружать только файлы с разрешением json.';
+    } else {
+
+        move_uploaded_file($_FILES['testfile']['tmp_name'], 'tests.json');
+
+        header('Location: list.php');
+
+        exit();
+    }
+}
+?>
 <!doctype html>
 <html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Тест</title>
-</head>
-<body>
-<h1>Загрузите файл с тестом</h1>
-<form action="list.php" enctype="multipart/form-data" method="post">
-    <p><input name="testfile" type="file"></p>
-    <input type="submit" value="Загрузить">
-</form>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Тест</title>
+    </head>
+    <body>
+        <h1>Загрузите файл с тестом</h1>
+        <form enctype="multipart/form-data" method="post">
+            <p><input name="testfile" type="file"></p>
+            <input type="submit" value="Загрузить">
+        </form>
+    </body>
 </html>
--- test.php
+++ test.php
@@ -1,48 +1,87 @@
 <?php
-    if (array_key_exists('testNumber', $_GET)) {
-        $data = json_decode(file_get_contents(__DIR__ . '/tests.json'), true);
-        $filter = FILTER_VALIDATE_INT; # задаем параметры фильтра
-        $options = ['options' => ['min_range' => 1,'max_range' => 4]];
-        $validate = filter_input(INPUT_GET, 'testNumber', $filter, $options);}
-?><!doctype html>
-<html lang="ru">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-<?php if ($validate):
-    $selectNumber = (int)$_GET['testNumber'];
-    $quest = $data[$selectNumber]['question'];
-    $answers = $data[$selectNumber]['answers'];
-    $correctAnswerNum = $data[$selectNumber]['correct_answer_num'] ?>
-    <h3>Выберите правильный ответ</h3>
-    <form method="post">
-        <p><b><?php echo $quest?></b> <br>
-            <?php foreach ($answers as $answerNum => $answer) : ?>
-                <label>
-                    <input type="radio" name="answer" value="<?php echo $answerNum?>">
-                </label> <?php echo $answer?>
-        </p>
-            <?php endforeach;?>
-        <input type="submit" value="Выбрать">
-    </form>
-<?php else:
-    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');?>
-    <h1>404 Not Found</h1>
-<?php endif; ?>
+$data = json_decode(file_get_contents(__DIR__ . '/tests.json'), true);
+if (!empty($_POST)) {
+    $q1 = $_POST['q1'];
+    $q2 = $_POST['q2'];
+    $q3 = $_POST['q3'];
+    if ($q1 == '' or $q2 == '' or $q3 == '') {
+        echo '<p>Вы не ответили на все вопросы. <a href="list.php">Назад</a></p>';
+        exit();
+    } else {
+        foreach ($data as $test) {
+            $correctAnswerNum1 = $test['question1']['correctAnswerNum'];
+            $correctAnswerNum2 = $test['question2']['correctAnswerNum'];
+            $correctAnswerNum3 = $test['question3']['correctAnswerNum'];
+        }
+        $score = 0;
+        if ($q1 == $correctAnswerNum1)
+            $score++;
+        if ($q2 == $correctAnswerNum2)
+            $score++;
+        if ($q3 == $correctAnswerNum3)
+            $score++;
+        echo 'Количество правильных ответов: <b>' . $score . '</b>'
+        . '<p><a href="list.php">Выбор теста</a></p>';
+        exit();
+    }
+}
 
-<?php if (!empty($_POST)):
-    $userAnswerNum = (int)++$_POST['answer'];
-    if($correctAnswerNum === $userAnswerNum): ?>
-        <b><p>Ответ правильный, тест пройден</p></b>
-    <?php else: ?>
-        <b><p>Ответ неправильный, тест не пройден</p></b>
-    <?php endif; ?>
-<?php endif; ?>
-
+if (array_key_exists('selectNumberTest', $_GET)) {
+    $filter = FILTER_VALIDATE_INT; # задаем параметры фильтра
+    $options = ['options' => ['min_range' => 1, 'max_range' => 4]];
+    $validate = filter_input(INPUT_GET, 'selectNumberTest', $filter, $options);
+    if (!$validate) {
+        header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+        echo '404 Not Found';
+        exit();
+    } else {
+        $selectNumberTest = (int) $_GET['selectNumberTest'];
+        foreach ($data as $NumberTest => $test) {
+            ++$NumberTest;
+            if ($selectNumberTest === $NumberTest) {
+                $quest1 = $test['question1']['question'];
+                $quest2 = $test['question2']['question'];
+                $quest3 = $test['question3']['question'];
+                $answers1 = $test['question1']['answers'];
+                $answers2 = $test['question2']['answers'];
+                $answers3 = $test['question3']['answers'];
+            }
+        }
+    }
+}
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Выберите правильный ответ:</h3>
+        <form  method="POST">
+            <fieldset>
+                <legend><?= $quest1 ?></legend>
+                <label><input name="q1" type="radio" value="1"><?= $answers1['0'] ?></label>
+                <label><input name="q1" type="radio" value="2"><?= $answers1['1'] ?></label>
+                <label><input name="q1" type="radio" value="3"><?= $answers1['2'] ?></label>
+                <label><input name="q1" type="radio" value="4"><?= $answers1['3'] ?></label>
+            </fieldset>
+            <fieldset>
+                <legend><?= $quest2 ?></legend>
+                <label><input name="q2" type="radio" value="1"><?= $answers2['0'] ?></label>
+                <label><input name="q2" type="radio" value="2"><?= $answers2['1'] ?></label>
+                <label><input name="q2" type="radio" value="3"><?= $answers2['2'] ?></label>
+                <label><input name="q2" type="radio" value="4"><?= $answers2['3'] ?></label>
+            </fieldset>
+            <fieldset>
+                <legend><?= $quest3 ?></legend>
+                <label><input name="q3" type="radio" value="1"><?= $answers3['0'] ?></label>
+                <label><input name="q3" type="radio" value="2"><?= $answers3['1'] ?></label>
+                <label><input name="q3" type="radio" value="3"><?= $answers3['2'] ?></label>
+                <label><input name="q3" type="radio" value="4"><?= $answers3['3'] ?></label>
+            </fieldset>
+            <input type="submit" value="Отправить">
+        </form>                    
+    </form>       
 </body>
-</html>
+</html>  
